90e942e4e57559317f602509f475d765f828175c
Shashwat Kumar <167195777+ShashwatSecure@users.noreply.github.com>
2025-12-27 08:16:11 +0000

    added favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon links - add a real favicon.ico into /public for best support */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.io.png" />
+        <link rel="apple-touch-icon" href="/favicon.io.png" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
